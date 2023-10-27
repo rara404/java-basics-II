cde6ea0f62ac28b8949943b0e0b904f6c1005e3a
Raj Rana <raj.rana@fiserv.com>
2023-10-27 14:04:45 -0400

    added Cat class and overrided toString for Cat

--- src/Inheritance/Cat.java
+++ src/Inheritance/Cat.java
@@ -1,24 +1,15 @@
 public class Cat {
-
-    // Data...
     private String name;
 
-    // Constructors...
     public Cat(String name) {
         this.name = name;
     }
 
-    // Methods...
     public String getName() {
         return name;
     }
 
-    @Override
     public String toString() {
         return "This is: " + name;
     }
-
-
-
-
 }

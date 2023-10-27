aac2191d59a619003e28c41736eea852a1dcfa03
Raj Rana <raj.rana@fiserv.com>
2023-10-27 13:50:49 -0400

    added the car class and tried toString with overriding

---
+++ Inheritance/src/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+
+    // Data...
+    private String name;
+
+    // Constructors...
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    // Methods...
+    public String getName() {
+        return name;
+    }
+
+
+
+}

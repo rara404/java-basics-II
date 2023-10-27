6664221380960d6a4de51f2d9a4a7e53dcae1917
Raj Rana <raj.rana@fiserv.com>
2023-10-27 14:49:05 -0400

    added stuff

--- src/Polymorphism/Account.java
+++ src/Polymorphism/Account.java
@@ -1,10 +1,10 @@
 public abstract class Account {
     protected double balance;
-    protected String accountHolder;
+    protected String account_Holder;
 
     public Account(double balance, String accountHolder) {
         this.balance = balance;
-        this.accountHolder = accountHolder;
+        this.account_Holder = accountHolder;
     }
 
     public abstract boolean withdraw(double amount);
